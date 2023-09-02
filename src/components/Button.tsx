2a8e66439ae460490a81725fa622f14d3235793f
ybarakat22 <ybarakat@identity-solutions.org>
2023-09-02 15:29:15 +0300

    Move js files to a dist folder

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 interface ButtonProps {
-    children:string;
-    color:string;
-    onClick:()=>void;
+  children: string;
+  color: string;
+  onClick: () => void;
 }
-const Button = ({children, color , onClick}:ButtonProps) => {
+const Button = ({ children, color, onClick }: ButtonProps) => {
   return (
-    <button type="button" className={"btn btn-"+ color} onClick={onClick}>
-       {children}
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
     </button>
   );
 };
